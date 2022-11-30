95fe820d3694fc20257f8e91138c71d44b679899
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-11-30 20:52:44 +0100

    model refactor

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,33 +1,40 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect } from "react";
 import css from "./Modal.module.css";
 import PropTypes from "prop-types";
 
-class Modal extends Component {
-	static propTypes = {
-		src: PropTypes.string.isRequired,
-		alt: PropTypes.string.isRequired,
-		closeHandler: PropTypes.func.isRequired,
-		escHandler: PropTypes.func.isRequired,
-	};
+const Modal = (props) => {
+	// componentDidMount() {
+	//
+	// }
 
-	componentDidMount() {
-		document.addEventListener("keydown", this.props.escHandler);
-	}
+	// componentWillUnmount() {
+	// 	document.removeEventListener("keydown", this.props.escHandler);
+	// }
 
-	componentWillUnmount() {
-		document.removeEventListener("keydown", this.props.escHandler);
-	}
+	useEffect(() => {
+		document.addEventListener("keydown", props.escHandler);
 
-	render() {
-		const { src, alt, closeHandler } = this.props;
-		return (
-			<div className={css["overlay"]} onClick={closeHandler}>
-				<div className={css["modal"]}>
-					<img src={src} alt={alt} />
-				</div>
+		return () => {
+			document.removeEventListener("keydown", props.escHandler);
+		};
+
+	}, []);
+
+	const { src, alt, closeHandler } = props;
+	return (
+		<div className={css["overlay"]} onClick={closeHandler}>
+			<div className={css["modal"]}>
+				<img src={src} alt={alt} />
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+Modal.propTypes = {
+	src: PropTypes.string.isRequired,
+	alt: PropTypes.string.isRequired,
+	closeHandler: PropTypes.func.isRequired,
+	escHandler: PropTypes.func.isRequired,
+};
 
 export default Modal;
